dc98898872e5ba7a86bafc6640636b5a4bd453b5
DevShah <bullet02@gmail.com>
2022-11-18 12:38:33 -0500

    Added missing migration fields updated/created/deleted at

---
+++ migrations/20221118171539-add-cols-updatedAtAnddeletedAtAndCreatedAt-to-usersAndorganizations-tbl.js
@@ -0,0 +1,110 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.addColumn(
+        'Organizations',
+        'createdAt',
+        {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.NOW,
+          validate: {
+            isDate: true
+          }
+        },
+        { transaction }
+      );
+      await queryInterface.addColumn(
+        'Organizations',
+        'updatedAt',
+        {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.NOW,
+          validate: {
+            isDate: true
+          }
+        },
+        { transaction }
+      );
+      await queryInterface.addColumn(
+        'Organizations',
+        'deletedAt',
+        {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.NOW,
+          validate: {
+            isDate: true
+          }
+        },
+        { transaction }
+      );
+
+      await queryInterface.addColumn(
+        'Users',
+        'createdAt',
+        {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.NOW,
+          validate: {
+            isDate: true
+          }
+        },
+        { transaction }
+      );
+      await queryInterface.addColumn(
+        'Users',
+        'updatedAt',
+        {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.NOW,
+          validate: {
+            isDate: true
+          }
+        },
+        { transaction }
+      );
+      await queryInterface.addColumn(
+        'Users',
+        'deletedAt',
+        {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.NOW,
+          validate: {
+            isDate: true
+          }
+        },
+        { transaction }
+      );
+
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+  },
+
+  async down(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.removeColumn('Users', 'createdAt', { transaction });
+      await queryInterface.removeColumn('Users', 'deletedAt', { transaction });
+      await queryInterface.removeColumn('Users', 'updatedAt', { transaction });
+      await queryInterface.removeColumn('Organizations', 'createdAt', { transaction });
+      await queryInterface.removeColumn('Organizations', 'deletedAt', { transaction });
+      await queryInterface.removeColumn('Organizations', 'updatedAt', { transaction });
+
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+  }
+};
