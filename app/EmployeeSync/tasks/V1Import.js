19ad5b50a99cd5b34ded10218a830c138cd59a5b
DevShah <bullet02@gmail.com>
2022-12-06 10:49:16 -0500

    Clean up import task comments and descriptors

--- app/EmployeeSync/tasks/V1Import.js
+++ app/EmployeeSync/tasks/V1Import.js
@@ -13,13 +13,8 @@ const Op = require('sequelize').Op; // for model operator aliases like $gte, $eq
 const io = require('socket.io-emitter')(REDIS_URL); // to emit real-time events to client-side applications: https://socket.io/docs/emit-cheatsheet/
 const joi = require('@hapi/joi'); // argument validations: https://github.com/hapijs/joi/blob/master/API.md
 const Queue = require('bull'); // add background tasks to Queue: https://github.com/OptimalBits/bull/blob/develop/REFERENCE.md#queueclean
-const moment = require('moment-timezone'); // manage timezone and dates: https://momentjs.com/timezone/docs/
-const convert = require('convert-units'); // https://www.npmjs.com/package/convert-units
 const slugify = require('slugify'); // convert string to URL friendly string: https://www.npmjs.com/package/slugify
-const sanitize = require('sanitize-filename'); // sanitize filename: https://www.npmjs.com/package/sanitize-filename
 const passport = require('passport'); // handle authentication: http://www.passportjs.org/docs/
-const currency = require('currency.js'); // handling currency operations (add, subtract, multiply) without JS precision issues: https://github.com/scurker/currency.js/
-const accounting = require('accounting'); // handle outputing readable format for currency: http://openexchangerates.github.io/accounting.js/
 const axios = require('axios');
 
 // services
@@ -31,9 +26,6 @@ const { ERROR_CODES, errorResponse, joiErrorsMessage } = require('../../../servi
 const models = require('../../../models');
 
 // helpers
-const { getOffset, getOrdering, convertStringListToWhereStmt } = require('../../../helpers/cruqd');
-const { randomString } = require('../../../helpers/logic');
-const { LIST_INT_REGEX } = require('../../../helpers/constants');
 
 // queues
 const EmployeeSyncQueue = new Queue('EmployeeSyncQueue', REDIS_URL);
@@ -49,15 +41,13 @@ module.exports = {
  * @job = {
  *   @id - (INTEGER - REQUIRED): ID of the background job
  *   @data = {
- *     @alpha - (STRING - REQUIRED): Alpha argument description
+ *     @organizationId - (INTEGER - REQUIRED): the organization which is syncing the employee data
  *   }
  * }
  *
- * Success: Return something
+ * Success: Resolve without error
  *
- * !IMPORTANT: This is an important message
- * !NOTE: This is a note
- * TODO: This is a todo
+ * TODO: Check to see if employees are already within our database
  */
 async function V1Import(job) {
   const schema = joi.object({
@@ -72,14 +62,11 @@ async function V1Import(job) {
   let { organizationId } = job.data;
 
   try {
-    /***** DO WORK HERE *****/
-
     let finchDirectoryUrl = 'https://api.tryfinch.com/employer/directory';
     let finchIndividualUrl = 'https://api.tryfinch.com/employer/individual';
     let organization = await models.organization.findByPk(organizationId, {
       attributes: ['hrisAccessToken']
     });
-    //'ddfd541f-c160-41f8-80cc-a0e355ba7dba';
 
     let resp = await axios.get(finchDirectoryUrl, {
       headers: {
@@ -110,8 +97,8 @@ async function V1Import(job) {
             roleType: 'EMPLOYEE',
             password: 'PLACEHOLDER',
             organizationId: organizationId,
-            addressline1: individual.body.residence.line1,
-            addressline2: individual.body.residence.line2,
+            addressLine1: individual.body.residence.line1,
+            addressLine2: individual.body.residence.line2,
             city: individual.body.residence.city,
             state: individual.body.residence.state,
             country: individual.body.residence.country,
