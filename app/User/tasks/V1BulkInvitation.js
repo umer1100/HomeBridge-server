652970e41a2a1066f2a324dc42f0bf7f327a4b35
Umer Akram <umer.akram3344@gmail.com>
2023-02-01 01:33:30 +0500

    Added functionality of Bulk Email Invitation (#39)
    * Completed functionality of Bulk Email Invitation

---
+++ app/User/tasks/V1BulkInvitation.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const { WEB_HOSTNAME } = process.env;
+const models = require('../../../models');
+const _ = require('lodash');
+const joi = require('@hapi/joi');
+const { joiErrorsMessage } = require('../../../services/error');
+const { randomString } = require('../../../helpers/logic');
+const bcrypt = require('bcrypt');
+const emailService = require('../../../services/email');
+
+module.exports = {
+  V1BulkInvitation
+};
+
+async function V1BulkInvitation(job) {
+  const schema = joi.object({
+    users: joi.array()
+    .required()
+    .error(new Error('Users array is not valid'))
+  });
+
+  const { error, value } = schema.validate(job.data);
+  if (error) return Promise.resolve(new Error(joiErrorsMessage(error)));
+  job.data = value;
+
+  let { users } = job.data;
+  users.forEach(async (user) => {
+    const userData = await models.user.findOne({
+      where: {
+        email: user.email,
+        status: 'PENDING'
+      }
+    });
+    if (userData) {
+      const passwordToSend = randomString({ len: 10 });
+      const password = bcrypt.hashSync(passwordToSend, userData.salt);
+      const emailConfirmationToken = randomString();
+      const emailConfirmLink = `${WEB_HOSTNAME}/ConfirmEmail?emailConfirmationToken=${emailConfirmationToken}&invitationEmail=${true}`;
+      await userData.update({
+        emailConfirmedToken: emailConfirmationToken,
+        password: password
+      });
+      await emailService.send({
+        from: emailService.emails.doNotReply.address,
+        name: emailService.emails.doNotReply.name,
+        subject: 'Please use this link to login to ownerific',
+        template: 'InvitationEmail',
+        tos: [user.email],
+        ccs: null,
+        bccs: null,
+        args: {
+          emailConfirmLink,
+          password: passwordToSend
+        }
+      });
+    }
+  })
+}
