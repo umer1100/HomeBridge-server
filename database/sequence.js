02c63d916f0d7f6b4431b7666e8afd894c131c7d
DevShah <bullet02@gmail.com>
2023-02-01 15:27:08 -0500

    Added models and migration for CreditWallet

--- database/sequence.js
+++ database/sequence.js
@@ -10,5 +10,6 @@ module.exports = [
   'organization',
   'user',
   'employeeSync',
-  'account'
+  'account',
+  'creditWallet'
 ];
---
+++ migrations/20230201202409-create-CreditWallet-model.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('CreditWallet', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+
+      userId: {
+        type: Sequelize.BIGINT,
+        allowNull: false
+      },
+
+      ownerificDollars: {
+        type: Sequelize.DECIMAL(20, 2),
+        allowNull: false,
+        defaultValue: 0.0,
+        validate: {
+          isDecimal: true
+        },
+        get() {
+          // convert string to float
+          const rawValue = this.getDataValue(example3);
+          return Number(rawValue);
+        }
+      },
+
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('NewModels');
+  }
+};
