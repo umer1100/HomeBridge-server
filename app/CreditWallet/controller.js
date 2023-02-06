aa7776f559b71d37ed98d7bdfe3cb0ee4a55d65d
DevShah <bullet02@gmail.com>
2023-02-06 08:55:01 -0500

    Added V1Read

--- app/CreditWallet/controller.js
+++ app/CreditWallet/controller.js
@@ -13,7 +13,8 @@ const { errorResponse, ERROR_CODES } = require('../../services/error');
 const actions = require('./actions');
 
 module.exports = {
-  V1Create
+  V1Create,
+  V1Read
 };
 
 /**
@@ -21,14 +22,39 @@ module.exports = {
  *
  * /v1/creditwallets/create
  *
- * Must be logged out | Must be logged in | Can be both logged in or logged out
- * Roles: ['admin', 'member']
+ * Must be logged in
+ * Roles: ['member']
  */
 async function V1Create(req, res, next) {
   let method = null; // which action method to use
 
   // Call the correct action method based on type of user of role
-  if (req.user) method = `V1ExampleByMember`;
+  if (req.user) method = `V1Create`;
+  else return res.status(401).json(errorResponse(req, ERROR_CODES.UNAUTHORIZED));
+
+  // call correct method
+  try {
+    const result = await actions[method](req);
+
+    return res.status(result.status).json(result);
+  } catch (error) {
+    return next(error);
+  }
+}
+
+/**
+ * Read Method
+ *
+ * /v1/creditwallets/read
+ *
+ * Must be logged in
+ * Roles: ['member']
+ */
+async function V1Read(req, res, next) {
+  let method = null; // which action method to use
+
+  // Call the correct action method based on type of user of role
+  if (req.user) method = `V1Read`;
   else return res.status(401).json(errorResponse(req, ERROR_CODES.UNAUTHORIZED));
 
   // call correct method
