987a9fdfe3d9ed487bd38aa41def232ff6b0316f
Umer Akram <umer.akram3344@gmail.com>
2023-02-09 17:50:11 +0500

    Fixed transfer to accounts data based on user role (#51)
    * Fix: Fetch transfer to accounts data based on user role
    
    * Incorporated feedback

--- app/Organization/actions/V1GetUsersPlaidAccountDetails.js
+++ app/Organization/actions/V1GetUsersPlaidAccountDetails.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const models = require('../../../models');
+const { isEmployer } = require('../../User/helper');
 
 module.exports = {
   V1GetUsersPlaidAccountDetails
 };
 
 /**
- * return all the plaid accounts associated with the organization of current user
+ * return all bank accounts if user is employer, else user's own bank accounts
  *
  * GET  /v1/organization/users-plaid-accounts
  * POST /v1/organization/users-plaid-accounts
@@ -22,12 +23,17 @@ module.exports = {
  */
 
 async function V1GetUsersPlaidAccountDetails(req) {
+  let options = !isEmployer(req.user) ? {
+    where: { userId: req.user.id },
+  } : {}
+
   try {
     let organizationData = await models.organization.findByPk(req.user.organizationId, {
       include: {
         model: models.user,
         include: {
-          model: models.plaidAccount
+          model: models.plaidAccount,
+          ...options
         }
       }
     })
@@ -36,21 +42,19 @@ async function V1GetUsersPlaidAccountDetails(req) {
       let { firstName, lastName, email } = user
       let data = user.plaidAccounts.map((acc) => {
         let { id, name, institutionName, accountId, itemId } = acc
-        if (req.user.id != acc.userId) {
-          return {
-            user: {
-              id: user.id,
-              firstName,
-              lastName,
-              email
-            },
-            id,
-            name,
-            institutionName,
-            accountId,
-            itemId,
-            label: ` ${firstName} ${lastName} | ${institutionName} | ${name}`
-          }
+        return {
+          user: {
+            id: user.id,
+            firstName,
+            lastName,
+            email
+          },
+          id,
+          name,
+          institutionName,
+          accountId,
+          itemId,
+          label: ` ${firstName} ${lastName} | ${institutionName} | ${name}`
         }
       })
       return data.filter(account => account).flat()
