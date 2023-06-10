2101732ced87536677a044c6694b3536062cf2ec
Dev Shah <DevShah@users.noreply.github.com>
2023-06-10 11:10:09 -0400

    Update/turn on employeesync (#106)
    * Needed to uncomment out missing import line

--- app/EmployeeSync/tasks/V1SyncAllOrganizations.js
+++ app/EmployeeSync/tasks/V1SyncAllOrganizations.js
@@ -57,7 +57,7 @@ async function V1SyncAllOrganizations(job) {
   organizations.forEach(organization => {
     (async () => {
       const currentRunId = await createSync(organization.id);
-      // await EmployeeSyncQueue.add('V1Import', { organizationId: organization.id, currentRunId: currentRunId });
+      await EmployeeSyncQueue.add('V1Import', { organizationId: organization.id, currentRunId: currentRunId });
     })();
   });
 
