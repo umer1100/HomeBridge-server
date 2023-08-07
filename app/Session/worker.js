5019c7f65bf76ad34efbb204f4f7f502b8bc617f
DevShah <bullet02@gmail.com>
2023-08-07 12:10:22 -0500

    TLS rejection on worker for Session object

--- app/Session/worker.js
+++ app/Session/worker.js
@@ -12,7 +12,7 @@ const { REDIS_URL } = process.env;
 
 // third party node modules
 const Queue = require('bull'); // process background tasks from Queue
-const SessionQueue = new Queue('SessionQueue', REDIS_URL);
+const SessionQueue = new Queue('SessionQueue', REDIS_URL, { redis: { tls: {rejectUnauthorized: false} }});
 
 
 // Returns an array of Queues used in this feature so we can gracefully close them in worker.js
