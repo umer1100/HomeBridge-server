3c4e890551ff3599652fc964ae3901564fb372f8
Umer Akram <umer.akram3344@gmail.com>
2023-08-24 12:45:42 -0400

    Removed unnecessery example queue that  cause memory leak (#127)

--- app/Questionaire/worker.js
+++ app/Questionaire/worker.js
@@ -7,16 +7,9 @@
 
 'use strict';
 
-// ENV variables
-const { REDIS_URL } = process.env;
-
-// third party node modules
-const Queue = require('bull'); // process background tasks from Queue
-const QuestionaireQueue = new Queue('QuestionaireQueue', REDIS_URL);
-
 // Returns an array of Queues used in this feature so we can gracefully close them in worker.js
 module.exports = () => {
 
   // return array of queues to the top level worker.js to gracefully close them
-  return [QuestionaireQueue];  // return empty array [] if not using any queues in this feature
+  return [];  // return empty array [] if not using any queues in this feature
 }
